4d8c434feeea7d0efeec1cdac25404bf784e3550
Showrav <showrabhasa715@gmail.com>
2023-02-27 08:42:46 +0600

    add

--- lib/reducer/ProductReducer.jsx
+++ lib/reducer/ProductReducer.jsx
@@ -6,15 +6,47 @@ const Reducer = (state, action) => {
       const featuredProducts = action.payload.slice(0, 8);
       const popular_products = action.payload.slice(0, 8);
       const special_offers = action.payload.slice(8, 12);
-      
+
+      let colors = [];
+      action.payload.map((product) => {
+        if (product.colors.length != 0) {
+          product.colors.map((color) => {
+            // update colors array
+            if (!colors.includes(color)) {
+              colors = [...colors, color];
+            }
+
+          })
+        }
+
+      });
+
       return {
         ...state,
         products: action.payload,
+        filter_products: action.payload,
         loading: false,
         featuredProducts,
         special_offers,
         popular_products,
+        colors,
       };
+
+    case "FILTER_PRODUCTS":
+      const { name, value } = action.payload;
+      const tempProducts = [...state.products];
+
+      if (name == "category") {
+        const filterProducts = tempProducts.filter(
+          (product) => {
+            return product.category_id == value
+          }
+        );
+        return { ...state, filter_products: filterProducts };
+      }
+
+
+
     case "ERROR":
       return { ...state, error: action.payload, loading: false };
     default:
